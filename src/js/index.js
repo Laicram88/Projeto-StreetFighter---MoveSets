48a25cb205cca5881fad6f4072a6e13bfe1e2a9e
Marcial Quaresma <marcial.quaresma@gmail.com>
2023-09-08 21:13:27 -0300

    Src
    alterando local de pastas, estilos e arquivos

--- src/js/index.js
+++ src/js/index.js
@@ -57,7 +57,7 @@ function alterarImagemPersonagemSelecionado(personagem) {
 
   // passo 2 - alterar a imagem do personagem grande
   const idPersonagem = personagem.attributes.id.value;
-  imagemPersonagemGrande.src = `./imagens/card-${idPersonagem}.png`;
+  imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
 }
 
 function removerSelecaoDoPersonagem() {
